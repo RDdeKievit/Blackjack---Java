a5389e34aeab2ed0072c2fe88f0713d9f29daa0c
Your Name <you@example.com>
2019-10-10 20:52:30 +0200

    Print random kaart uit dek

--- Blackjack.java
+++ Blackjack.java
@@ -1,9 +1,16 @@
+
+import java.util.Random;
 class Blackjack {
 	
-	public static int dekSpeelkaarten[] = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
-
+	public static int[] dekSpeelkaarten = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+	
 	public static void main (String args[]){
-		System.out.println(dekSpeelkaarten.length);
+		System.out.println(getKaart(dekSpeelkaarten));
+	}
+
+	public static int getKaart(int[] array) {
+   		int rnd = new Random().nextInt(array.length);
+		return array[rnd];
 	}
 
 }
