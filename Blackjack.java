3d03006f7bd6d354afaecce4f65d2f5ef0c8a5e5
Your Name <you@example.com>
2019-10-10 20:29:42 +0200

    Dek speelkaarten gecreeerd

---
+++ Blackjack.java
@@ -0,0 +1,9 @@
+class Blackjack {
+	
+	public static int dekSpeelkaarten[] = new int[] {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
+	public static void main (String args[]){
+		System.out.println(dekSpeelkaarten.length);
+	}
+
+}
